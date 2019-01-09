eb00575ba34070d4629ea39e6625091842e26b54
Naoya Inoue@Whiskey <naoya-i@ecei.tohoku.ac.jp>
2019-01-09 09:47:25 +0900

    New results.

--- results.md
+++ results.md
@@ -2,7 +2,8 @@ BEST:
 
 Earlystopping = 15, Bidirectional_GRU
 
-############################################## TN16
+# Baselines
+## TN16
 
 Best hyperparameter based on fold no. 2 and 4 among 5 folds:
 
@@ -36,7 +37,9 @@ clipnorm 5, dropout 0.7 = val_loss: 0.0493
 clipnorm 3, dropout 0.7 = val_loss: 0.0490
 clipnorm 3, dropout 0.9 = val_loss: 0.0541
 
+```
 CUDA_VISIBLE_DEVICES=0 python src/train.py  --fold 0   --model-type nea --dropout 0.7    --embedding-dim 50 --aggregation-grudim 300  --gradientclipnorm 3 --meanovertime   --pre-trained --fix-embedding
+```
 
 fold_0: MSE: 0.3621767404762582, MAE: 0.43679335951805115
 fold_1: MSE: 0.3298523836700365, MAE: 0.41286392235637304
@@ -44,9 +47,10 @@ fold_2: MSE: 0.3931328914678891, MAE: 0.42098302983525976
 fold_3: MSE: 0.30799082403575084,MAE: 0.42356242943758987
 fold_4: MSE: 0.36748199519640024,MAE: 0.46359519481658934
 
-******** MSE: 0.352127
+- MSE: 0.352127
 
-############################################################ TN16+PN10
+
+## TN16+PN10
 
 Best hyperparameter based on fold no. 2 and 4 among 5 folds:
 
@@ -72,7 +76,9 @@ clipnorm 0, dropout 0.7 = val_loss: 0.0203
 clipnorm 5, dropout 0.9 = val_loss: 0.0181
 clipnorm 3, dropout 0.7 = val_loss: 0.0205
 
+```
 CUDA_VISIBLE_DEVICES=0 python src/train.py     --fold 0     --model-type nea --dropout 0.9     --embedding-dim 50 --aggregation-grudim 300     --gradientclipnorm 5 --meanovertime     --pre-trained --fix-embedding     --persing-seq --pseq-embedding-dim 16 --pseq-encoder-dim 400
+```
 
 fold_0: MSE: 0.18817686358418173, MAE: 0.35129995703697203
 fold_1: MSE: 0.18391588820501945, MAE: 0.3496616467907654
@@ -80,4 +86,102 @@ fold_2: MSE: 0.1780143176682396, MAE: 0.35123343906592375
 fold_3: MSE: 0.20512577761926526, MAE: 0.3599928285352033
 fold_4: MSE: 0.2213006920121294, MAE: 0.3716801816225052
 
-******* MSE: 0.1953067
+- MSE: 0.1953067
+
+
+# Document encoder pretraining
+
+## With DI shuffling
+
+### Pretraining
+
+```
+{'aggr_grudim': '300', 'att': 'False', 'clipnorm': '5.0', 'dropout': '0.7', 'emb_dim': '50', 'emb_fix': 'False', 'enc_fix': 'False', 'encdim': 'None', 'model_type': 'nea', 'mot': 'True', 'pretrained': 'False', 'shuf': 'di'}
+```
+
+val_acc: 0.879913
+
+
+### TN16+PN10+pretrain (di. shuffle, no fine-tuning)
+
+- MSEs: 
+- MSE:
+
+
+### TN16+PN10+pretrain (di. shuffle, with fine-tuning)
+
+- MSEs: 
+- MSE: 
+
+
+## With sentence shuffling
+
+### Pretraining
+
+```
+aggr_grudim=300
+att=False
+clipnorm=5.0
+dropout=0.7
+emb_dim=50
+emb_fix=False
+enc_fix=False
+encdim=None
+model_type=nea
+mot=True
+pretrained=False
+shuf=sentence
+```
+
+val_acc: 0.732533
+
+
+### TN16+PN10+pretrain (sent. shuffle, no fine-tuning)
+
+```
+aggr_grudim=300
+att=False
+clipnorm=5.0
+di_aware=False
+dropout=0.7
+emb_dim=50
+emb_fix=True
+enc_fix=True
+encdim=None
+model_type=nea_aft_pretrain
+mot=True
+preenc=output_enc/c2c4d855a06224fd1096834eed11920d
+pretrained=False
+pseq=True
+pseq_embdim=16
+pseq_encdim=400
+```
+
+- MSEs: [0.1708993219855502, 0.188276010211909, 0.16168191047724936, 0.21673573230273174, 0.21641181262379064]
+- MSE: (0.1908009575202462, STDEV: 0.022710780694247448)	
+
+
+### TN16+PN10+pretrain (sent. shuffle, with fine-tuning)
+
+```
+aggr_grudim=300
+att=False
+clipnorm=5.0
+di_aware=False
+dropout=0.7
+emb_dim=50
+emb_fix=False
+enc_fix=False
+encdim=None
+model_type=nea_aft_pretrain
+mot=True
+preenc=output_enc/c2c4d855a06224fd1096834eed11920d
+pretrained=False
+pseq=True
+pseq_embdim=16
+pseq_encdim=400
+```
+
+- MSEs: [0.17768359446192292, 0.16614793271124353, 0.1609080225191747, 0.20891687420427643, 0.21897310227394462]
+- MSE: 0.18652590523411244, STDEV: 0.023254677920348094
+
--- src/bulkrun.py
+++ src/bulkrun.py
@@ -5,37 +5,37 @@ comm_eval = ["""
 # TN16
 python src/eval.py \
     --fold {} \
-    --model-dir output/988c82dd83f6a34df8005ac89db85465
+    --model-dir output/
 """,
 """
 # TN16+PN10
 python src/eval.py \
     --fold {} \
-    --model-dir output/2966a197dd56de18a9973720c9219a0d
+    --model-dir output/
 """,
 """
 # TN16+PN10+pretrain(di. shuffle, fixed)
 python src/eval.py \
     --fold {} \
-    --model-dir output/eb40209496004f29ca30acb28e49c665
+    --model-dir output/e7066f606904451be43dff3562cd06e1
 """,
 """
 # TN16+PN10+pretrain(di. shuffle, not fixed)
 python src/eval.py \
     --fold {} \
-    --model-dir output/e0ab2e7b43b37719666bb6e1b0e58e10
+    --model-dir output/057450e4e7e49a11c90f4fdc03e1de01
 """,
 """
 # TN16+PN10+pretrain(sent. shuffle, fixed)
 python src/eval.py \
     --fold {} \
-    --model-dir output/fa37bf66f2563eec16382c1eac16a108
+    --model-dir output/752aa6160e706a6ffe0f91a1e423b40a
 """,
 """
 # TN16+PN10+pretrain(sent. shuffle, not fixed)
 python src/eval.py \
     --fold {} \
-    --model-dir output/45209d4279bbad14c42f65bbc19aa2f3
+    --model-dir output/0cb7a2429b6cc73297413c20570c824f
 """,
 ]
 
@@ -63,43 +63,62 @@ python src/train.py \
 python src/train.py \
     --fold {} \
     --model-type nea_aft_pretrain --dropout 0.7 \
-    --embedding-dim 50 --aggregation-grudim 100 \
+    --embedding-dim 50 --aggregation-grudim 300 \
     --gradientclipnorm 5 --meanovertime \
-    --persing-seq --pseq-embedding-dim 16 --pseq-encoder-dim 64 \
+    --persing-seq --pseq-embedding-dim 16 --pseq-encoder-dim 400 \
     --fix-encoder --fix-embedding \
-    --pretrained-encoder output_enc/a87b827fa7c5151192542ecb2c3af4d2
+    --pretrained-encoder output_enc/9780456c95e7c048e2501106fd40c716
 """,
 """
 # TN16+PN10+pretrain(di. shuffle, not fixed)
 python src/train.py \
     --fold {} \
     --model-type nea_aft_pretrain --dropout 0.7 \
-    --embedding-dim 50 --aggregation-grudim 100 \
+    --embedding-dim 50 --aggregation-grudim 300 \
     --gradientclipnorm 5 --meanovertime \
-    --persing-seq --pseq-embedding-dim 16 --pseq-encoder-dim 64 \
-    --pretrained-encoder output_enc/a87b827fa7c5151192542ecb2c3af4d2
+    --persing-seq --pseq-embedding-dim 16 --pseq-encoder-dim 400 \
+    --pretrained-encoder output_enc/9780456c95e7c048e2501106fd40c716
 """,
 """
 # TN16+PN10+pretrain(sent. shuffle, fixed)
 python src/train.py \
     --fold {} \
     --model-type nea_aft_pretrain --dropout 0.7 \
-    --embedding-dim 50 --aggregation-grudim 100 \
+    --embedding-dim 50 --aggregation-grudim 300 \
     --gradientclipnorm 5 --meanovertime \
-    --persing-seq --pseq-embedding-dim 16 --pseq-encoder-dim 64 \
+    --persing-seq --pseq-embedding-dim 16 --pseq-encoder-dim 400 \
     --fix-encoder --fix-embedding \
-    --pretrained-encoder output_enc/750570aed2d16633ecbe4237d2d95b71
+    --pretrained-encoder output_enc/c2c4d855a06224fd1096834eed11920d
 """,
 """
 # TN16+PN10+pretrain(sent. shuffle, not fixed)
 python src/train.py \
     --fold {} \
     --model-type nea_aft_pretrain --dropout 0.7 \
-    --embedding-dim 50 --aggregation-grudim 100 \
+    --embedding-dim 50 --aggregation-grudim 300 \
     --gradientclipnorm 5 --meanovertime \
-    --persing-seq --pseq-embedding-dim 16 --pseq-encoder-dim 64 \
-    --pretrained-encoder output_enc/750570aed2d16633ecbe4237d2d95b71
+    --persing-seq --pseq-embedding-dim 16 --pseq-encoder-dim 400 \
+    --pretrained-encoder output_enc/c2c4d855a06224fd1096834eed11920d
+""",
+]
+
+comm_train_enc = [
 """
+# Pretraining with sentence-based shuffling
+python src/train_enc.py \
+    --model-type nea --dropout 0.7 \
+    --embedding-dim 50 --aggregation-grudim 300 \
+    --gradientclipnorm 5 --meanovertime \
+    --shuffle-type sentence
+""",
+"""
+# Pretraining with DI-based shuffling
+python src/train_enc.py \
+    --model-type nea --dropout 0.7 \
+    --embedding-dim 50 --aggregation-grudim 300 \
+    --gradientclipnorm 5 --meanovertime \
+    --shuffle-type di
+""",
 ]
 
 if sys.argv[2] == "train":
@@ -114,9 +133,22 @@ elif sys.argv[2] == "train_allfolds":
     f = int(sys.argv[1])
     comm = [comm_train[f].format(i) for i in range(0, 5)]
 
+elif len(sys.argv) == 4 and sys.argv[3] == "train_onefold":
+    f, ff = int(sys.argv[1]), int(sys.argv[2])
+    comm = [comm_train[f].format(ff)]
+    
 elif sys.argv[2] == "eval_allfolds":
     f = int(sys.argv[1])
     comm = [comm_eval[f].format(i) for i in range(0, 5)]
+    
+elif sys.argv[2] == "train_enc":
+    f = int(sys.argv[1])
+    comm = [comm_train_enc[f]]
+    
+elif len(sys.argv) == 4 and sys.argv[3] == "eval_onefold":
+    f, ff = int(sys.argv[1]), int(sys.argv[2])
+    comm = [comm_eval[f].format(ff)]
+    
         
 for c in comm:
     print("===")
